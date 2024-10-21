b4ba9f4b53c3c72eb287bae3a5fe8a2ebf33e37a
Vanessa Guerrero <vguerrero@appdev.yearup.org>
2024-10-21 14:19:35 -0500

    Created hello.py

---
+++ hello.py
@@ -0,0 +1,15 @@
+print('Hello world!')
+
+
+message = 'Hello world!'
+print(message)
+
+dollars = 3
+cents = .50
+
+print (dollars + cents)
+
+print(type(dollars + cents)) #float
+print(type(dollars)) #integer so the cents is a float
+
+print('3 dollars' + ' ' + 'fifty cents')
