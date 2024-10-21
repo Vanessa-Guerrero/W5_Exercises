bd8812001a273149fb4625b8d1c448ddf8fb3467
Vanessa Guerrero <vguerrero@appdev.yearup.org>
2024-10-21 15:29:12 -0500

    Created variable examples file

---
+++ variable_examples.py
@@ -0,0 +1,36 @@
+customer_id = 1 #int
+customer_name = ' Mary Jane Doe' #string
+gender = 'female' #string
+dob = '1/1/1990' #string
+drivers_lic_num = 'G123456' #string
+auto_policy_num = 'ABC123' #string
+
+my_name = 'Vanessa Guerrero'
+city_state = 'Queens, NY'
+
+
+# Question 1: What is the full list of reserved words that can’t be used for variable names?
+
+ 
+ # False, def, if, raise, None, del, import, return, True, elif, in, try, and, else, is, while, as, except,
+ # lambda, with, assert, finally, nonlocal, yield, break, for, not, class, form, or, continue, global, pass
+
+import keyword
+print (keyword.kwlist)
+
+
+
+# Question 2: Pick 5 of these words and review the explanation for how it is used as a keyword in Python. 
+# Add these 5 definitions as comments to your exercise document. 
+# Put ^^ around any terms that you are not familiar with?
+ 
+ # ^^def^^ : used to define a function or a method in Python
+
+ # if: An if statement is used to make a conditional statement. If the condition is True, then some action is performed.
+
+ # None: (captial N in None )
+ # There is no null value in Python. The None is an object that represents the absence of a value. It is like an empty object
+
+ # while: used to start a while loop. It continues iteration until a condition is no longer True
+
+ # ^^lambda^^: anonymous function. It can take any number of arguments but only have a single expression
