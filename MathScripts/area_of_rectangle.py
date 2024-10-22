07b4fa6e14334355746fac74db5dcfe9dd7e667f
Vanessa Guerrero <vguerrero@appdev.yearup.org>
2024-10-22 11:24:48 -0500

    Created area_of _rectangle.py file

---
+++ MathScripts/area_of_rectangle.py
@@ -0,0 +1,9 @@
+# Define known values
+length = 5
+width = 8
+
+# Calculate the unknown
+area_of_rectangle = length * width
+
+# Display the results
+print("The area of the rectangle is " + str(area_of_rectangle))
