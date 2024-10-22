ae5fc6a85a7f83b525fda076506bcbd05e8296d8
Vanessa Guerrero <vguerrero@appdev.yearup.org>
2024-10-22 12:00:55 -0500

    Created area_of_circle.py file

---
+++ MathScripts/area_of_circle.py
@@ -0,0 +1,12 @@
+# Importing math module to use the math.pi constant for pi
+import math
+
+# Define known values
+radius = 3
+pi = math.pi
+
+# Calculate the unknown
+area_of_circle = pi * radius ** 2
+
+# Display the results
+print("The area of a circle with radius " + str(radius) + " is " + ('{:.2f}'.format(area_of_circle)))
