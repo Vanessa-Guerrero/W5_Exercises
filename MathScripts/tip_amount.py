3ff82939f3d1e57db32f20f1ca8f1745b2ed7bfa
Vanessa Guerrero <vguerrero@appdev.yearup.org>
2024-10-22 11:40:23 -0500

    Created tips_amount.py file

---
+++ MathScripts/tip_amount.py
@@ -0,0 +1,9 @@
+# Define known values
+food_cost = 55
+tip_perc = .20
+
+# Calculate the unknown
+tip_amount = food_cost * tip_perc
+
+# Display the results
+print("The tip on a $" + str(food_cost) + " restaurant bill is $" + ('{:.2f}'.format(tip_amount)))
