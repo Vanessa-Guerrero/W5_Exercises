4e487a0195ae1e6babcafaadd57c2d5f9647a57f
Vanessa Guerrero <vguerrero@appdev.yearup.org>
2024-10-22 11:19:20 -0500

    Modified net_worth.py file

--- MathScripts/net_worth.py
+++ MathScripts/net_worth.py
@@ -1,2 +1,11 @@
 # Define known values
-assets = 
+assets = 1500
+debts = 450.50
+
+# Calculate the unknown
+net_worth = assets - debts
+
+# Display the results
+print("Your net worth is " + str(net_worth))  # without commas
+
+print("Your net worth is " + ('{:,.2f}'.format(net_worth))) # with commas and 2 decimal place
