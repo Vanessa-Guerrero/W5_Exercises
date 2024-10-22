6d9e283740f94f74a6351529730aece93184c1cf
Vanessa Guerrero <vguerrero@appdev.yearup.org>
2024-10-22 16:33:02 -0500

    Created rule of 72 file

---
+++ MathScripts/rule _of_72.py
@@ -0,0 +1,9 @@
+interest_rate = .08
+current_savings_acct = 15000
+
+rule_of_72 = 72 / (interest_rate*100) # rule to find how many years it would take to approx double you amount
+years = rule_of_72 # 8 years
+
+future_savings_account = current_savings_acct*(1+interest_rate)**years # future savings account balance
+
+print ("At a " + format(interest_rate, ".0%") + " interest rate, your savings account will be worth " + ("{:,.2f}".format(future_savings_account)) + " in " + format(years, ".1f") + " years")
