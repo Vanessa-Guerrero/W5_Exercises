ff6eecf65bfb80361e3dfd7abe543518d9d37766
Vanessa Guerrero <vguerrero@appdev.yearup.org>
2024-10-23 15:55:36 -0500

    Add and complete taxes.py exercise

--- MathScripts/taxes.py
+++ MathScripts/taxes.py
@@ -1,8 +1,8 @@
 # Question 3. Federal taxes are 23% of your salary every month. You make X amount of money. 
  # How much is withheld for taxes?
 
-salary = int(input('What is your monthly gross salary? '))
+salary = int(input('What is your monthly gross salary? $'))
 
-taxes = round((Salary * .23), 2)
+taxes = (salary * .23)
 
-print(str(Taxes))
+print('The amount of taxes withheld from your monthly salary is $' + format(taxes, ".2f"))
