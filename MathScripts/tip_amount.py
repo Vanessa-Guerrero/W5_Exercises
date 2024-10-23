4cf2b0c06152c852a40172c761872e4381299ae2
Vanessa Guerrero <vguerrero@appdev.yearup.org>
2024-10-23 17:33:16 -0500

    Complete Bonus #1 for Ex. 2.A

--- MathScripts/tip_amount.py
+++ MathScripts/tip_amount.py
@@ -2,10 +2,10 @@
 
 # Define known values
 food_cost = 55
-tip_perc = .20
+tip_perc = float(input("How much would you like to give tip in percentage? "))      # Bonus 1
 
 # Calculate the unknown
-tip_amount = food_cost * tip_perc
+tip_amount = food_cost * (tip_perc/100)
 
 # Display the results
 print("The tip on a $" + str(food_cost) + " restaurant bill is $" + ('{:.2f}'.format(tip_amount)))
