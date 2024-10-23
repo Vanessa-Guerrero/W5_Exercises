f65b9322e1ef77b0b7b41a125c27c433b1afb56d
Vanessa Guerrero <vguerrero@appdev.yearup.org>
2024-10-23 14:40:54 -0500

    Add and update c_to_f.py file

--- MathScripts/c_to_f.py
+++ MathScripts/c_to_f.py
@@ -1,7 +1,7 @@
-# Question 1. How do you convert a temperature from Fahrenheit to Celsius?
+# Question 2. How do you convert a temperature from Celsius to Farenheit?
 
 Celsius = int(input('What is the temperature in Celsius? '))
 
 Farenheit = round((9/5) * Celsius + 32)
 
-print('The temperature converted to Celsius is ' + str(Celsius))
+print('The temperature converted to Farenheit is ' + str(Farenheit))
