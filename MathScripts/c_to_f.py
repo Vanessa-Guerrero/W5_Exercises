74612206f2bac66f1f4c872e1664a321c1c7dd02
Vanessa Guerrero <vguerrero@appdev.yearup.org>
2024-10-23 15:51:24 -0500

    Update c_to_fy.py file

--- MathScripts/c_to_f.py
+++ MathScripts/c_to_f.py
@@ -1,7 +1,7 @@
 # Question 2. How do you convert a temperature from Celsius to Farenheit?
 
-Celsius = int(input('What is the temperature in Celsius? '))
+celsius = int(input('What is the temperature in Celsius? '))
 
-Farenheit = round((9/5) * Celsius + 32)
+farenheit = round((9/5) * celsius + 32)
 
-print('The temperature converted to Farenheit is ' + str(Farenheit))
+print('The temperature converted to Farenheit is ' + str(farenheit))
---
+++ MathScripts/taxes.py
@@ -0,0 +1,8 @@
+# Question 3. Federal taxes are 23% of your salary every month. You make X amount of money. 
+ # How much is withheld for taxes?
+
+salary = int(input('What is your monthly gross salary? '))
+
+taxes = round((Salary * .23), 2)
+
+print(str(Taxes))
